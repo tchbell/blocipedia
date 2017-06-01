ba0a6523ca38a79ecfcaa9575af2c60bca2a56b2
Ted <tchbell@gmail.com>
2017-06-01 02:07:33 +0000

    Added CRUD to wiki

--- spec/controllers/wikis_controller_spec.rb
+++ spec/controllers/wikis_controller_spec.rb
@@ -18,8 +18,18 @@ RSpec.describe WikisController, type: :controller do
 
   describe "GET #show" do
     it "returns http success" do
-      get :show
+      get :show, {id: my_wiki.id}
       expect(response).to have_http_status(:success)
+    end
+    
+    it "renders the view" do
+      get :show, {id: my_wiki.id}
+      expect(response).to render_template :show
+    end
+    
+    it "assigns my_wiki to @wiki" do
+      get :show, {id: my_wiki.id}
+      expect(assigns(:wiki)).to eq(my_wiki)
     end
   end
 
@@ -28,13 +38,89 @@ RSpec.describe WikisController, type: :controller do
       get :new
       expect(response).to have_http_status(:success)
     end
+    
+    it "renders the #new view" do
+      get :new
+      expect(response).to render_template :new
+    end
+    
+    it "instantiates @wiki" do
+      get :new
+      expect(assigns(:wiki)).not_to be_nil
+    end
+  end
+  
+  describe "POST #create" do
+    it "increases the number of Wiki by 1" do
+      expect{post :create, wiki:{title: "Here is a title", body: "This is a body for our wiki"}}.to change(Wiki,:count).by(1)
+    end
+    
+    it "assigns the new wiki to Wiki" do
+      post :create, wiki:{title: "Here is a title", body: "This is a body for our wiki"}
+      expect(assigns(:wiki)).to eq Wiki.last
+    end
+    
+    it "redirects to the new wiki" do
+      post :create, wiki:{title: "Here is a title", body: "This is a body for our wiki" }
+    expect(response).to redirect_to Wiki.last
+    end
   end
 
   describe "GET #edit" do
     it "returns http success" do
-      get :edit
+      get :edit, {id: my_wiki.id}
       expect(response).to have_http_status(:success)
+    end
+    
+    it "renders the #edit view" do
+      get :edit, {id: my_wiki.id}
+      expect(response).to render_template :edit
+    end
+    
+    it "assigns wiki to be updated to @wiki" do
+      get :edit, {id: my_wiki.id}
+      
+      wiki_instance = assigns(:wiki)
+      
+      expect(wiki_instance.id).to eq my_wiki.id
+      expect(wiki_instance.title).to eq my_wiki.title
+      expect(wiki_instance.body).to eq my_wiki.body
+    end
+  end
+  
+  describe "PUT #update" do
+   it "updates wiki with expected attributes" do
+       new_title = "NEW TITLE"
+       new_body = "RANDOM WIKI BODY TEST TEXT"
+ 
+       put :update, id: my_wiki.id, wiki: {title: new_title, body: new_body}
+ 
+       updated_wiki = assigns(:wiki)
+       expect(updated_wiki.id).to eq my_wiki.id
+       expect(updated_wiki.title).to eq new_title
+       expect(updated_wiki.body).to eq new_body
+     end
+    
+    it "redirect_to the updated post" do
+      new_title = "NEW TITLE"
+      new_body = "NEW BODY GOES IN HERE YAY!"
+      
+      put :update, id: my_wiki.id, wiki: {title: new_title, body: new_body }
+      expect(response).to redirect_to my_wiki
     end
   end
 
+
+  describe "DELETE #destroy" do
+    it "deletes the wiki" do
+      delete :destroy, {id: my_wiki.id}
+      count = Wiki.where({id: my_wiki.id}).size
+      expect(count).to eq 0
+    end
+    
+    it "redirects the wiki index" do 
+      delete :destroy, {id: my_wiki.id}
+      expect(response).to redirect_to wikis_path
+    end
+  end
 end
